82c8ba32e32596db1297d7816ac71a5a2a5c55f5
charleston <charles1235223@gmail.com>
2023-02-26 16:56:51 -0400

    first commit

---
+++ bin/fuction_dart_imc_01.dart
@@ -0,0 +1,5 @@
+import 'package:fuction_dart_imc_01/fuction_dart_imc_01.dart' as fuction_dart_imc_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fuction_dart_imc_01.calculate()}!');
+}
---
+++ test/fuction_dart_imc_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:fuction_dart_imc_01/fuction_dart_imc_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
